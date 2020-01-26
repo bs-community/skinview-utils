c26e4f9676c91146731e3ef02d32d0dc96557a7e
yushijinhun <yushijinhun@gmail.com>
2020-01-27 03:00:18 +0800

    Add utils for creating viewer object

---
+++ src/load-image.ts
@@ -0,0 +1,24 @@
+export type RemoteImage = string | {
+    src: string;
+    crossOrigin?: string | null;
+    referrerPolicy?: string;
+};
+
+export async function loadImage(source: RemoteImage): Promise<HTMLImageElement> {
+    const image = document.createElement("img");
+    return new Promise((resolve, reject) => {
+        image.onload = (): void => resolve(image);
+        image.onerror = reject;
+        if (typeof source === "string") {
+            image.src = source;
+        } else {
+            if (source.crossOrigin !== undefined) {
+                image.crossOrigin = source.crossOrigin;
+            }
+            if (source.referrerPolicy !== undefined) {
+                image.referrerPolicy = source.referrerPolicy;
+            }
+            image.src = source.src;
+        }
+    });
+}
