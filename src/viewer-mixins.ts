c05b8e4097ecd75b775745ca1ab8d950717e024b
yushijinhun <yushijinhun@gmail.com>
2020-06-13 21:17:02 +0800

    loadSkin/Cape now accepts null

--- src/viewer-mixins.ts
+++ src/viewer-mixins.ts
@@ -11,19 +11,23 @@ export function applyMixins(derivedCtor: any, baseCtors: any[]): void {
 	});
 }
 
-export function isTextureSource(value: TextureSource | RemoteImage): value is TextureSource {
+function isTextureSource(value: TextureSource | RemoteImage): value is TextureSource {
 	return value instanceof EventTarget || value instanceof ImageBitmap;
 }
 
 export abstract class SkinContainer<T> {
 	protected abstract skinLoaded(model: ModelType, options?: T): void;
 	protected abstract get skinCanvas(): TextureCanvas;
+	protected abstract resetSkin(): void;
 
+	loadSkin(empty: null): void;
 	loadSkin(source: TextureSource, model: ModelType | "auto-detect", options?: T): void;
 	async loadSkin(source: RemoteImage, model: ModelType | "auto-detect", options?: T): Promise<void>;
 
-	loadSkin(source: TextureSource | RemoteImage, model: ModelType | "auto-detect" = "auto-detect", options?: T): void | Promise<void> {
-		if (isTextureSource(source)) {
+	loadSkin(source: TextureSource | RemoteImage | null, model: ModelType | "auto-detect" = "auto-detect", options?: T): void | Promise<void> {
+		if (source === null) {
+			this.resetSkin();
+		} else if (isTextureSource(source)) {
 			loadSkinToCanvas(this.skinCanvas, source);
 			const actualModel = model === "auto-detect" ? inferModelType(this.skinCanvas) : model;
 			this.skinLoaded(actualModel, options);
@@ -36,12 +40,16 @@ export abstract class SkinContainer<T> {
 export abstract class CapeContainer<T> {
 	protected abstract capeLoaded(options?: T): void;
 	protected abstract get capeCanvas(): TextureCanvas;
+	protected abstract resetCape(): void;
 
+	loadCape(empty: null): void;
 	loadCape(source: TextureSource, options?: T): void;
 	async loadCape(source: RemoteImage, options?: T): Promise<void>;
 
-	loadCape(source: TextureSource | RemoteImage, options?: T): void | Promise<void> {
-		if (isTextureSource(source)) {
+	loadCape(source: TextureSource | RemoteImage | null, options?: T): void | Promise<void> {
+		if (source === null) {
+			this.resetCape();
+		} else if (isTextureSource(source)) {
 			loadCapeToCanvas(this.capeCanvas, source);
 			this.capeLoaded(options);
 		} else {
